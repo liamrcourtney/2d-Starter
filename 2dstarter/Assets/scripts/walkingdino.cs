237cfd87d400902261f0013efca15b85ca1a844e
liamrcourtney <liamcdaa@gmail.com>
2020-10-29 11:39:21 +0200

    scenes and scripts

---
+++ 2dstarter/Assets/scripts/walkingdino.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class walkingdino : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Character speed: " + speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+
+        
+    }
+}
