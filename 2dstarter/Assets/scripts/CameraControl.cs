57132892cc90cb4c90a6d5aebb97d450e7726287
liamrcourtney <liamcdaa@gmail.com>
2020-10-31 17:49:13 +0200

    Camera Control

---
+++ 2dstarter/Assets/scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform DinoTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(DinoTransform.position.x, transform.position.y, transform.position.z);
+    }
+}
